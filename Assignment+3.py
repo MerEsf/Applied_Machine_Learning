4b104ae085b688d3455aef8e1002cb62c9ff4b9d
Mehrshad Esfahani <Mehrshad.1990@gmail.com>
2019-01-19 21:51:10 -0700

    Update Assignment+3.py

--- Assignment+3.py
+++ Assignment+3.py
@@ -1,32 +1,15 @@
 
-# coding: utf-8
 
-# ---
-# 
-# _You are currently looking at **version 1.2** of this notebook. To download notebooks and datafiles, as well as get help on Jupyter notebooks in the Coursera platform, visit the [Jupyter Notebook FAQ](https://www.coursera.org/learn/python-machine-learning/resources/bANLa) course resource._
-# 
-# ---
-
-# # Assignment 3 - Evaluation
-# 
-# In this assignment you will train several models and evaluate how effectively they predict instances of fraud using data based on [this dataset from Kaggle](https://www.kaggle.com/dalpozz/creditcardfraud).
 #  
-# Each row in `fraud_data.csv` corresponds to a credit card transaction. Features include confidential variables `V1` through `V28` as well as `Amount` which is the amount of the transaction. 
-#  
-# The target is stored in the `class` column, where a value of 1 corresponds to an instance of fraud and 0 corresponds to an instance of not fraud.
-
-# In[43]:
+# Each row in `fraud_data.csv` corresponds to a credit card transaction. 
+# Features include confidential variables `V1` through `V28` as well as `Amount` which is the amount of the transaction.  
+# The target is stored in the `class` column, where a value of 1 corresponds to an instance of fraud and 
+# 0 corresponds to an instance of not fraud.
 
 import numpy as np
 import pandas as pd
 
-
-# ### Question 1
-# Import the data from `fraud_data.csv`. What percentage of the observations in the dataset are instances of fraud?
-# 
-# *This function should return a float between 0 and 1.* 
-
-# In[44]:
+#  Calculate the percentage of the observations in the dataset that are instances of fraud
 
 def answer_one():
     
@@ -34,17 +17,8 @@ def answer_one():
     ans = sum(df['Class']) / len(df['Class'])
     
     return ans
-#answer_one()
 
 
-# In[ ]:
-
-
-
-
-# In[45]:
-
-# Use X_train, X_test, y_train, y_test for all of the following questions
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('fraud_data.csv')
@@ -55,18 +29,8 @@ y = df.iloc[:,-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 
-# In[ ]:
-
-
-
-
-# ### Question 2
-# 
-# Using `X_train`, `X_test`, `y_train`, and `y_test` (as defined above), train a dummy classifier that classifies everything as the majority class of the training data. What is the accuracy of this classifier? What is the recall?
-# 
-# *This function should a return a tuple with two floats, i.e. `(accuracy score, recall score)`.*
-
-# In[46]:
+# train a dummy classifier that classifies everything as the majority class of the training data. 
+# Calculate the accuracy of this classifier and the recall?
 
 def answer_two():
     from sklearn.dummy import DummyClassifier
@@ -78,21 +42,10 @@ def answer_two():
     recall_score = recall_score(y_test, y_dummy_predictions)
     
     return (accuracy_score, recall_score)
-#answer_two()
 
+#  train a SVC classifer using the default parameters
+# calculate the accuracy, recall, and precision
 
-# In[ ]:
-
-
-
-
-# ### Question 3
-# 
-# Using X_train, X_test, y_train, y_test (as defined above), train a SVC classifer using the default parameters. What is the accuracy, recall, and precision of this classifier?
-# 
-# *This function should a return a tuple with three floats, i.e. `(accuracy score, recall score, precision score)`.*
-
-# In[47]:
 
 def answer_three():
     from sklearn.metrics import recall_score, precision_score, accuracy_score
@@ -105,16 +58,9 @@ def answer_three():
     precision_score = precision_score(y_test, predictions)
     
     return (accuracy_score, recall_score, precision_score)
-#answer_three()
 
-
-# ### Question 4
-# 
-# Using the SVC classifier with parameters `{'C': 1e9, 'gamma': 1e-07}`, what is the confusion matrix when using a threshold of -220 on the decision function. Use X_test and y_test.
-# 
-# *This function should return a confusion matrix, a 2x2 numpy array with 4 integers.*
-
-# In[49]:
+# Using the SVC classifier with parameters `{'C': 1e9, 'gamma': 1e-07}`, 
+# Find the confusion matrix when using a threshold of -220 on the decision function. 
 
 def answer_four():
     from sklearn.metrics import confusion_matrix
@@ -125,22 +71,13 @@ def answer_four():
     confusion_matrix = confusion_matrix(y_test, svm_predicted)
     
     return confusion_matrix
-#answer_four()
 
+# Train a logisitic regression classifier with default parameters 
 
-# ### Question 5
-# 
-# Train a logisitic regression classifier with default parameters using X_train and y_train.
-# 
-# For the logisitic regression classifier, create a precision recall curve and a roc curve using y_test and the probability estimates for X_test (probability it is fraud).
-# 
 # Looking at the precision recall curve, what is the recall when the precision is `0.75`?
 # 
 # Looking at the roc curve, what is the true positive rate when the false positive rate is `0.16`?
-# 
-# *This function should return a tuple with two floats, i.e. `(recall, true positive rate)`.*
 
-# In[52]:
 
 def answer_five():
 
@@ -170,32 +107,7 @@ def answer_five():
 
 answer_five()
 
-
-# ### Question 6
-# 
 # Perform a grid search over the parameters listed below for a Logisitic Regression classifier, using recall for scoring and the default 3-fold cross validation.
-# 
-# `'penalty': ['l1', 'l2']`
-# 
-# `'C':[0.01, 0.1, 1, 10, 100]`
-# 
-# From `.cv_results_`, create an array of the mean test scores of each parameter combination. i.e.
-# 
-# |      	| `l1` 	| `l2` 	|
-# |:----:	|----	|----	|
-# | **`0.01`** 	|    ?	|   ? 	|
-# | **`0.1`**  	|    ?	|   ? 	|
-# | **`1`**    	|    ?	|   ? 	|
-# | **`10`**   	|    ?	|   ? 	|
-# | **`100`**   	|    ?	|   ? 	|
-# 
-# <br>
-# 
-# *This function should return a 5 by 2 numpy array with 10 floats.* 
-# 
-# *Note: do not return a DataFrame, just the values denoted by '?' above in a numpy array. You might need to reshape your raw result to meet the format we are looking for.*
-
-# In[51]:
 
 def answer_six():    
     from sklearn.model_selection import GridSearchCV
@@ -208,19 +120,13 @@ def answer_six():
     ans = np.array(grid_lr.cv_results_['mean_test_score'].reshape(5,2))
     
     return ans
-#answer_six()
 
-
-# In[42]:
-
-# Use the following function to help visualize results from the grid search
 #def GridSearch_Heatmap(scores):
     get_ipython().magic('matplotlib notebook')
     import seaborn as sns
     import matplotlib.pyplot as plt
-    # plt.figure()
-    # sns.heatmap(scores.reshape(5,2), xticklabels=['l1','l2'], yticklabels=[0.01, 0.1, 1, 10, 100])
-    # plt.yticks(rotation=0);
+    lt.figure()
+    sns.heatmap(scores.reshape(5,2), xticklabels=['l1','l2'], yticklabels=[0.01, 0.1, 1, 10, 100])
+    plt.yticks(rotation=0);
 
-#GridSearch_Heatmap(answer_six())
 
